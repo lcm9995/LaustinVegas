1be6d2b500634f4e90fe17c91d22d1ce82abefdf
lcm9995 <112994511+lcm9995@users.noreply.github.com>
2025-07-12 16:10:01 -0400

    logo fixed

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -12,15 +12,15 @@ export default function Header() {
 
   return (
     <div className="flex flex-col items-center px-4 pt-12 sm:pt-8">
-      <div className="flex justify-center mt-0 mb-10 sm:mb-6 md:mb-5">
+      <div className="flex justify-center mt-0 mb-12 sm:mb-6 md:mb-7">
         <img 
-          src="/Asset 11.svg" 
+          src="/Asset 17.svg" 
           alt="Laustin Vegas Logo" 
           className="w-[600px] h-[180px] lg:w-[600px] lg:h-[180px] sm:w-[200px] sm:h-[160px] md:w-[300px] md:h-[300px] object-fill"
         />
       </div>
 
-      <div className="flex justify-center space-x-8 sm:space-x-5 mt-0 mb-0 text-med uppercase font-semibold tracking-widest">
+      <div className="flex justify-center lg:space-x-10 md:space-x-8 sm:space-x-5 mt-0 lg:mt-4 mb-0 lg:text-med md:text-med sm:text-sm uppercase font-semibold tracking-widest">
         {navItems.map((item) => (
           <a
             key={item.href}
