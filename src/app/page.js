e1342601cbeb4e0f434a96183e335664cf560e92
lcm9995 <112994511+lcm9995@users.noreply.github.com>
2025-07-12 23:08:08 -0400

    idk

--- src/app/page.js
+++ src/app/page.js
@@ -19,18 +19,18 @@ export default function Home() {
       ></iframe>
 
       <div className="w-full max-w-4xl flex justify-center gap-6 mt-10 mb-8">
-        <a href="https://open.spotify.com/artist/58kIjokyAq51h6Imc7htQG?si=n2QX4XpoT4akcW_qmfEttA" target="_blank" rel="noopener noreferrer">
+        <Link href="https://open.spotify.com/artist/58kIjokyAq51h6Imc7htQG?si=n2QX4XpoT4akcW_qmfEttA" target="_blank" rel="noopener noreferrer">
           <FaSpotify className="w-8 h-8 text-white grayscale hover:grayscale-0 transition" />
-        </a>
-        <a href="https://music.apple.com/us/artist/laustin-vegas/1714944338" target="_blank" rel="noopener noreferrer">
+        </Link>
+        <Link href="https://music.apple.com/us/artist/laustin-vegas/1714944338" target="_blank" rel="noopener noreferrer">
           <FaApple className="w-8 h-8 text-white grayscale hover:grayscale-0 transition" />
-        </a>
-        <a href="https://www.youtube.com/@LaustinVegas" target="_blank" rel="noopener noreferrer">
+        </Link>
+        <Link href="https://www.youtube.com/@LaustinVegas" target="_blank" rel="noopener noreferrer">
           <FaYoutube className="w-8 h-8 text-white grayscale hover:grayscale-0 transition" />
-        </a>
-        <a href="https://tidal.com/browse/artist/52121431?u" target="_blank" rel="noopener noreferrer">
+        </Link>
+        <Link href="https://tidal.com/browse/artist/52121431?u" target="_blank" rel="noopener noreferrer">
           <SiTidal className="w-8 h-8 text-white grayscale hover:grayscale-0 transition" />
-        </a>
+        </Link>
       </div>
     </div>
   );
