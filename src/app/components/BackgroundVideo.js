78f6bb8e1476cf2ca597fed0b986167f2315fdeb
lcm9995 <112994511+lcm9995@users.noreply.github.com>
2025-07-17 20:41:26 -0400

    vid mp4

--- src/app/components/BackgroundVideo.js
+++ src/app/components/BackgroundVideo.js
@@ -7,7 +7,7 @@ export default function BackgroundVideo() {
         playsInline
         className="fixed top-0 left-0 w-full h-full object-cover -z-10"
       >
-        <source src="/background.mp4" type="video/mp4" />
+        <source src="/paradisee.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     );
