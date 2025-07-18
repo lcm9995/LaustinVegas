c633963f1ceb03a5e6e985f7a4fc273abd10e117
lcm9995 <112994511+lcm9995@users.noreply.github.com>
2025-07-17 20:36:37 -0400

    video first try

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import './globals.css';
 import { Analytics } from "@vercel/analytics/next"
 import Sidebar from './components/Sidebar';
+import BackgroundVideo from './components/BackgroundVideo';
 import BorderFrame from './components/BorderFrame';
 
 export const metadata = {
@@ -23,23 +24,28 @@ export default function RootLayout({ children }) {
 
       </head>
       <body className="bg-black text-white flex flex-col min-h-screen">
-        <BorderFrame />
-  
-        <>
-          {/* Left Border */}
-          <div className="fixed top-4 bottom-12 left-10 w-px bg-white pointer-events-none z-50" aria-hidden="true" />
-          
-          {/* Right Border */}
-          <div className="fixed top-4 bottom-12 right-10 w-px bg-white pointer-events-none z-50" aria-hidden="true" />
-       
-        </>
-      
-        <main className="flex-1">{children} 
-        <Analytics />
-        </main>
-        <footer className="text-center p-4 text-xs text-white border-t bg-black border-black">
-          © {new Date().getFullYear()} Laustin Vegas. All rights reserved.
-        </footer>
+        <BackgroundVideo />
+        <div className="fixed top-0 left-0 w-full h-full bg-black opacity-40 z-0" />
+
+        <div className = "relative z-10">
+          <BorderFrame />
+    
+          <>
+            {/* Left Border */}
+            <div className="fixed top-4 bottom-12 left-10 w-px bg-white pointer-events-none z-50" aria-hidden="true" />
+            
+            {/* Right Border */}
+            <div className="fixed top-4 bottom-12 right-10 w-px bg-white pointer-events-none z-50" aria-hidden="true" />
+        
+          </>
+        
+          <main className="flex-1">{children} 
+          <Analytics />
+          </main>
+          <footer className="text-center p-4 text-xs text-white border-t bg-black border-black">
+            © {new Date().getFullYear()} Laustin Vegas. All rights reserved.
+          </footer>
+        </div>
       </body>
     </html>
   );
